f154f38b26dc1f7dd7814b5c8371313816a9f94a
angibeom0985-arch <angibeom0985@gmail.com>
2026-01-08 07:23:54 +0900

    디버그 기본 경로 리디렉션 제거

--- index.tsx
+++ index.tsx
@@ -28,7 +28,7 @@ root.render(
         <Route path="/guide" element={<GuidePage />} />
         <Route path="/api-guide" element={<ApiGuidePage />} />
         <Route path="/admin" element={<AdminPage />} />
-        <Route path="/debug" element={<Navigate to="/debug/script" replace />} />
+        <Route path="/debug" element={<DebugPage mode="script" />} />
         <Route path="/debug/script" element={<DebugPage mode="script" />} />
         <Route path="/debug/image/*" element={<DebugPage mode="image" />} />
         <Route path="/download-progress" element={<DownloadProgressPage />} />
